fa6d7456fdd351ab79f846046df95f261e7ade2f
AntoineTohan <antoine.durand2@epsi.fr>
2020-04-14 16:15:45 +0200

    Add first route for users and integrate Mongoose and MOngoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { UsersModule } from './user/users.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pap-back'),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,9 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  age: Number,
+  email: String,
+  password: String,
+});
